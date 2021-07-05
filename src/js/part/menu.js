2a21ab899a37c39a7780fafda94af6bb4882ba77
SergeySolovev <name.diesel@gmail.com>
2021-07-05 18:54:59 +0300

    Доработка меню. Scrollto

--- src/js/part/menu.js
+++ src/js/part/menu.js
@@ -2,11 +2,24 @@
 
     let menuBth = document.querySelector('.js-menu-sandwich');
     let menuItems = document.querySelector('.js-menu-items');
-    function menuTogglen() {
+    let menuItem = document.querySelectorAll('.js-menu-items a');
+    function menuToggle() {
         slideToggle(menuItems);
+        menuBth.classList.toggle("menu__btn--active");
     }
 
     menuBth.addEventListener('click', function (e) {
-        menuTogglen()
+        menuToggle();
     })
+
+    Array.prototype.forEach.call(menuItem, function (el, i) {
+        el.addEventListener('click', function (e) {
+            let targetId = e.target.getAttribute('href');
+            menuToggle();
+            if (document.querySelector(targetId)) {
+                e.preventDefault();
+                scrollIt(document.querySelector(targetId), 1000, 'easeOutQuad');
+            }
+        })
+    });
 })();
